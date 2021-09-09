ed693f3b833f6135c997102a5a69e8835d4bbe16
eugeneby108 <456123>
2021-09-09 13:34:10 +0300

    ServDepTest

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,24 +5,28 @@ namespace App\Http\Controllers;
 
 
 use App\Http\Requests\RegisterRequest;
-
-
 use App\Models\User;
+use App\Services\UserService;
 use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
-    public function register(RegisterRequest $request)
+    protected $userService;
+
+    public function __construct(UserService $userService)
     {
-
-
-        $user = User::create([
-            'email' => $request->email,
-            'password' => $request->password
-        ]);
-
-        $token = $user->createToken('LaravelAuthApp')->accessToken;
-
-        return response()->json(['token' => $token], 201);
+        $this->userService = $userService;
     }
+
+
+
+    public function register (RegisterRequest $request){
+        $this->userService->createUser($request);
+
+    }
+
+
+
+
+
 }
