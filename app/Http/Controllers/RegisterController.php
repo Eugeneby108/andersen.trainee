33f527ed8d972dfb39c706bd4ba102ab38f2469c
eugeneby108 <456123>
2021-09-17 16:38:50 +0300

    Auth: reset password

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -21,6 +21,7 @@ use Illuminate\Support\Str;
 use Illuminate\Contracts\Auth\CanResetPassword;
 use Laravel\Passport\Passport;
 use Illuminate\Auth\Passwords\PasswordBroker;
+use function Composer\Autoload\includeFile;
 
 class RegisterController extends Controller
 {
@@ -61,7 +62,17 @@ class RegisterController extends Controller
     public function resetPassword(ResetRequest $request)
     {
         $user = User::where('email', $request->email)->first();
+        $token = ResetPassword::where('token', $request->token)->first();
+        if($token){
+            return response()->json(['token error' => 'Token is already exist'], 400);
+        }
+
+        if($token->created_at->copy()->addHours(2)->isPast()){
+            $token->delete();
+            return response()->json(['token error' => 'Try to get it again'], 404);
+        }
         $token = str::random(60);
+
         ResetPassword::create([
             'user_id' => $user->id,
             'email' => $user->email,
@@ -70,20 +81,23 @@ class RegisterController extends Controller
 
         $mail = $request->email;
         Mail::to($mail)->send(new \App\Mail\PasswordReset($token));
-        return ('Email is sending');
+        return response('Email is sending succesfully');
     }
 
     public function newPassword(NewPasswordRequest $request)
     {
         $token = ResetPassword::where('token', $request->token)->first();
-        if($token->created_at->copy()->addMinutes(2)->isPast()){
-            return response()->json(['token error' => 'Try to get it again']);
+        if($token->created_at->copy()->addHours(2)->isPast()){
+            $token->delete();
+            return response()->json(['token error' => 'Try to get it again'], 404);
         }
-        $id = ResetPassword::where('token', $request->token)->value('user_id');
+
+        $id = $token->user_id;
         $user = User::findOrFail($id);
         $newPassword = $request->password;
         $user->fill(['password' => bcrypt($newPassword)]);
         $user->save();
-        ResetPassword::where('token', $request->token)->delete();
+        return response('New password has been saved');
+
     }
 }
