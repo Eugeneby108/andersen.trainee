3c8bce0e871b7161014fa33a533f1b6b858ab537
eugeneby108 <456123>
2021-09-17 11:37:23 +0300

    Auth: reset password

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Models\ResetPassword;
+use App\Models\User;
 use App\Services\UserService;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Password as RulesPassword;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Contracts\Auth\CanResetPassword;
+use Laravel\Passport\Passport;
+use Illuminate\Auth\Passwords\PasswordBroker;
 
 class RegisterController extends Controller
 {
@@ -18,16 +32,17 @@ class RegisterController extends Controller
     public function register(RegisterRequest $request)
     {
         $data = [
-        'email' => $request->email,
-        'password' => $request->password
-    ];
+            'email' => $request->email,
+            'password' => $request->password
+        ];
 
         $user = $this->userService->createUser($data);
 
         return response(['token' => $this->userService->token], 201);
     }
 
-    public function login(RegisterRequest $request){
+    public function login(RegisterRequest $request)
+    {
         $credentials = [
             'email' => $request->email,
             'password' => $request->password
@@ -39,5 +54,36 @@ class RegisterController extends Controller
         } else {
             return response()->json(['error' => 'UnAuthorised'], 401);
         }
+    }
+
+    public function resetPassword(RegisterRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        $token = str::random(60);
+        ResetPassword::create([
+            'user_id' => $user->id,
+            'email' => $user->email,
+            'token' => $token
+        ]);
+        $mail = $request->email;
+
+        Mail::to($mail)->send(new \App\Mail\PasswordReset($token));
+    }
+
+    public function newPassword(RegisterRequest $request)
+    {
+        $newToken = ResetPassword::where('email', $request->email)->value('token','created_at');
+        $newPassword = $request->password;
+        $user = new User([
+            'email' => $request->email,
+            'password' => Hash::make($newPassword),
+        ]);
+
+        ResetPassword::where('email', $request->email)->delete();
+
+        if(!$newToken){
+            $this->resetPassword($request);
+        }
+
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 
+use Carbon\Carbon;
 use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -29,6 +30,6 @@ class AuthServiceProvider extends ServiceProvider
 
         Passport::routes();
 
-
+        Passport::tokensExpireIn(Carbon::now()->addHours(2));
     }
 }
